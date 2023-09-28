2a64c860296f88e9928f9e319c45842e06814a3d
Nathan Litzinger <nlitz88@gmail.com>
2023-09-27 23:55:21 -0400

    Added implementation for getting average depth of gap and unit tests

--- gap_follow/gap_follow/gap_follow_utils.py
+++ gap_follow/gap_follow/gap_follow_utils.py
@@ -292,11 +292,7 @@ def get_gap_max_depth(ranges: List[float], gap_left_index: int, gap_right_index:
         float: The maximum range value found in the subset of ranges
         [gap_left_index:gap_right_index].
     """
-    max = 0
-    for i in range(gap_left_index, gap_right_index+1):
-        if ranges[i] > max:
-            max = ranges[i]
-    return max
+    return max(ranges[gap_left_index:gap_right_index+1])
 
 def get_gap_average_depth(ranges: List[float], gap_left_index: int, gap_right_index: int) -> float:
     """Gets the average depth found in the ranges array between the provided
@@ -311,7 +307,7 @@ def get_gap_average_depth(ranges: List[float], gap_left_index: int, gap_right_in
         float: The average of the range values in the subset of ranges
         [gap_left_index:gap_right_index].
     """
-    return 0.0
+    return sum(ranges[gap_left_index:gap_right_index+1])/len(ranges[gap_left_index:gap_right_index+1])
 
 def get_gap_middle_index(gap_left_index: int, gap_right_index: int) -> int:
     """Gets the (approximate) middle most index of the provided gap within a
--- gap_follow/gap_follow/tests/test_gap_follow_utils.py
+++ gap_follow/gap_follow/tests/test_gap_follow_utils.py
@@ -550,5 +550,34 @@ class TestGetGapMaxDepth(unittest.TestCase):
         result = get_gap_max_depth(ranges, gap_left_index, gap_right_index)
         self.assertEqual(result, 4.0)
 
+class TestGetGapAverageDepth(unittest.TestCase):
+    def test_basic_case_with_gap_in_middle(self):
+        ranges = [1.0, 2.0, 3.0, 0.5, 2.5, 1.0, 4.0]
+        gap_left_index = 3
+        gap_right_index = 5
+        result = get_gap_average_depth(ranges, gap_left_index, gap_right_index)
+        self.assertAlmostEqual(result, 1.3333, places=2)
+
+    def test_gap_of_size_1(self):
+        ranges = [1.0, 2.0, 3.0, 0.5, 2.5, 1.0, 4.0]
+        gap_left_index = 3
+        gap_right_index = 3
+        result = get_gap_average_depth(ranges, gap_left_index, gap_right_index)
+        self.assertEqual(result, 0.5)
+
+    def test_gap_entire_range(self):
+        ranges = [1.0, 2.0, 3.0, 0.5, 2.5, 1.0, 4.0]
+        gap_left_index = 0
+        gap_right_index = 6
+        result = get_gap_average_depth(ranges, gap_left_index, gap_right_index)
+        self.assertAlmostEqual(result, 2.0, places=4)
+
+    def test_gap_at_beginning(self):
+        ranges = [0.5, 2.5, 1.0, 4.0, 1.0, 2.0, 3.0]
+        gap_left_index = 0
+        gap_right_index = 3
+        result = get_gap_average_depth(ranges, gap_left_index, gap_right_index)
+        self.assertAlmostEqual(result, 2.0, places=2)
+
 if __name__ == "__main__":
     unittest.main()
