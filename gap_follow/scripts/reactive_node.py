2bf320d3cc274a0d38b0dbbc22e061ab185c99aa
Nathan Litzinger <nlitz88@gmail.com>
2023-09-28 01:44:27 -0400

    Added dummy script to execute reactive node

---
+++ gap_follow/scripts/reactive_node.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Dummy script used to execute node in reactive_node.py within gap_follow
+python package.
+"""
+
+from gap_follow import reactive_node
+
+if __name__=="__main__":
+    reactive_node.main()
